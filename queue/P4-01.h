5a73557a1190a2c627a4ecdf0444598ba368ab92
yeoseunghyun <onyshup@gmail.com>
2018-11-02 15:43:14 +0900

    by seunghyun

---
+++ queue/P4-01.h
@@ -0,0 +1,27 @@
+//	queue ADT Type Defintions 
+typedef struct node
+{
+	void*        dataptr;
+	struct node* next;
+} queuenode;
+typedef struct
+{
+	queuenode* front;
+	queuenode* rear;
+	int         count;
+} queue;
+
+//	Prototype Declarations 
+queue* createqueue(void);
+queue* destroyqueue(queue* queue);
+
+bool  dequeue(queue* queue, void** itemptr);
+bool  enqueue(queue* queue, void*  itemptr);
+bool  queuefront(queue* queue, void** itemptr);
+bool  queuerear(queue* queue, void** itemptr);
+int   queuecount(queue* queue);
+
+bool  emptyqueue(queue* queue);
+bool  fullqueue(queue* queue);
+void  printqueue(queue* queue);
+//	End of queue ADT Definitions 
