80d705491409b1d2058ebd3e2a9535f26f126054
yeoseunghyun <onyshup@gmail.com>
2018-11-04 00:54:28 +0900

    simplest

--- queue/P4-11.h
+++ queue/P4-11.h
@@ -1,28 +1,13 @@
 bool requeue(queue* pqueue){
 
-	queuenode* newptr;
-	
-	if(!(newptr = 
-		(queuenode*)malloc(sizeof(queuenode))))
-		return false;
-	
-	void* tempdata;
-	tempdata = pqueue->front->dataptr;
-	
-	newptr->dataptr = tempdata;
-	newptr->next = NULL;
+	queuenode* tempptr;
 
-	if(pqueue->count == 0)
-		pqueue->front = newptr;
-	else{
-		
-		pqueue->rear->next = newptr;
-		pqueue->front = pqueue->front->next;
-		//pqueue->front->next = NULL;
-		free(pqueue->front->next);
-		pqueue->front->next = NULL;
-		pqueue->rear = newptr;
-	}	
-	pqueue->rear = newptr;
-	return true;
+	if(pqueue->count ==0)
+		return 0;
+
+	tempptr = pqueue->front;
+	pqueue->front = pqueue->front->next;
+	pqueue->rear->next = tempptr;
+	pqueue->rear=pqueue->rear->next;
+	
 }
